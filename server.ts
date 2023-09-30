90454b79bac90ee011585ffff5a2235a6269a5a8
bedpleyar <bedpleyar@gmail.com>
2023-09-30 18:57:57 +0100

    Hello world for Express

--- server.ts
+++ server.ts
@@ -1,1 +1,15 @@
 console.log("Hello");
+
+const PORT = 3000;
+const express = require("express");
+const app = express();
+
+// routes
+app.get('/', (req, res) => {
+    res.send("Hello Node API");
+});
+
+app.listen(PORT, () => {
+    console.log(`Node API app is running on ${PORT}`);
+
+});
