10f2195decef548080367b2e67a9b52a798ffa2b
bedpleyar <bedpleyar@gmail.com>
2023-10-01 01:54:05 +0100

    Add ts-node and db connection code

--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
+import { error } from "console";
+
 console.log("Hello");
 
 const PORT = 3000;
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 // routes
@@ -13,3 +16,10 @@ app.listen(PORT, () => {
     console.log(`Node API app is running on ${PORT}`);
 
 });
+
+mongoose.connect('mongodb+srv://admin:Supergreatpassword1%21@pdrahelper.joeldgn.mongodb.net/Portfolio?retryWrites=true&w=majority',)
+.then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) =>
+    console.log(error)
+);
