9e66ae076771371433823ac2dc3ad7db572bf26f
bedpleyar <bedpleyar@gmail.com>
2023-10-04 21:21:33 +0100

    added base grid settings, added fetch fn

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -3,30 +3,34 @@ import ProjectDisplay from './ProjectDisplay';
 
 function HomePage() {   
     return (
-        <div className="body">
-            <div>
-                <div style={{marginBottom : "50px"}}>
-                <Typography variant="h2">
-                    Hi, I'm Louis.
-                </Typography>
-                </div>
-                <Typography variant="body1">
-                    This page is dedicated to my portfolio and a small "About Me" page.
-                    <br/>
-                    <br/> Below you may find out more information about the projects I've worked on and the tech they have involved.
-                    <br/> For further information and ways to reach out to myself please consult the "About Me" page.
-                </Typography>
-                <div style={{marginTop: "50px", marginBottom: "25px"}}>
-                <Typography variant="h4">
-                    Projects
-                </Typography>
-                </div>     
+        <div>
+            <div className="body">
                 <div>
+                    <div style={{marginBottom : "50px"}}>
+                    <Typography variant="h2">
+                        Hi, I'm Louis.
+                    </Typography>
+                    </div>
+                    <Typography variant="body1">
+                        This site is dedicated to my portfolio and a small "About Me" page.
+                        <br/>
+                        <br/> Below you may find out more information about the projects I've worked on and the tech they have involved.
+                        <br/> For further information and ways to reach out to myself please consult the "About Me" page.
+                    </Typography>
+                    <div style={{marginTop: "50px", marginBottom: "25px"}}>
+                    <Typography variant="h4">
+                        Projects
+                    </Typography>
+                    </div>     
+                </div>     
+
+            </div>
+            <div>
+                <div style={{margin: "35px", marginLeft: "25%", marginRight: "25%"}}>
                     {/*TODO: Add grid of projects pulled from API*/}
                     <ProjectDisplay />
                 </div>
-            </div>     
-
+            </div>
         </div>
         
         
--- client/src/components/ProjectDisplay.tsx
+++ client/src/components/ProjectDisplay.tsx
@@ -1,8 +1,41 @@
+import Grid from '@mui/material/Unstable_Grid2';
+import Paper from '@mui/material/Paper';
+import "../App.css"
+import { useQuery } from '@tanstack/react-query';
+import { Typography } from '@mui/material';
 
+async function fetchProjects() {
+    const res = await fetch("/project")
+    return res.json()
+}
 
 function ProjectDisplay() {
+    const {data : projects, isLoading} = useQuery({
+        queryFn: () => fetchProjects(),
+        queryKey: ["projects"]
+    })
+
+    if (isLoading) {
+        return (
+            <Typography variant='body1' align='center'>Loading projects...</Typography>
+        )
+    }
+
     return (
-            <p></p>
+        <Grid container spacing={2}>
+            <Grid xs={12} sm={6} md={6} lg={3}>
+            <Paper elevation={6} >xs=6 md=8</Paper>
+            </Grid>
+            <Grid xs={12} sm={6} md={6} lg={3}>
+            <Paper elevation={6} >xs=6 md=4</Paper>
+            </Grid>
+            <Grid xs={12} sm={6} md={6} lg={3}>
+            <Paper elevation={6} >xs=6 md=4</Paper>
+            </Grid>
+            <Grid xs={12} sm={6} md={6} lg={3}>
+            <Paper elevation={6} >xs=6 md=8</Paper>
+            </Grid>
+        </Grid>
     )
 }
 
