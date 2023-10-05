4f99e97545215c9a9541fe5b49bfa2ee0fa0d9d1
bedpleyar <bedpleyar@gmail.com>
2023-10-05 03:09:13 +0100

    add delete functionality to api

--- server/src/server.ts
+++ server/src/server.ts
@@ -53,7 +53,22 @@ app.put("/project/:id", async (req: any, res: any) => {
 
     res.status(200).json(updatedProject)
   } catch (error: any) {
+    console.log(error.message)
+    res.status(500).json({message : error.message})
+  }
+})
 
+app.delete("/project/:id", async (req: any, res: any) => {
+  try {
+    const {id} = req.params;
+    const project = await Project.findByIdAndDelete(id)
+    if (!project) {
+      res.status(404).json({message: `Cannot find object with id ${id}`})
+    }
+    res.status(200)
+  } catch (error: any) {
+    console.log(error.message)
+    res.status(500).json({message : error.message})
   }
 })
 
