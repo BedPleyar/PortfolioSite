570102167a1986d58b35990e9b598e185cde2835
bedpleyar <bedpleyar@gmail.com>
2023-10-05 01:04:51 +0100

    Added admin controls and refactored adminpage

---
+++ client/src/components/AdminControls.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material';
+import '../App.css';
+
+function AdminControls() {
+
+
+    return (
+        <div className='body'>
+            <Typography variant="h4">Admin Controls</Typography>
+        </div>
+    )
+}
+
+
+export default AdminControls
