20d01a125243be8bdf333cae4661b77a78dc8058
bedpleyar <bedpleyar@gmail.com>
2023-10-05 01:04:36 +0100

    Added admin controls and refactored adminpage

--- client/src/components/AdminPage.tsx
+++ client/src/components/AdminPage.tsx
@@ -3,6 +3,7 @@ import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { red } from '@mui/material/colors';
+import AdminControls from './AdminControls';
 
 
 function Admin() {
@@ -31,56 +32,60 @@ function Admin() {
     }
 
     return (
-            <div className="body">
-                <div style={{marginBottom : "50px"}}>
-                    <Typography variant="h2">Admin Page</Typography>
+            <div>
+                {loggedIn ? <AdminControls /> : 
+                <div className="body">
+                    <div style={{marginBottom : "50px"}}>
+                        <Typography variant="h2">Admin Page</Typography>
+                    </div>
+
+                    <div style={{marginBottom : "15px"}}>
+                        <TextField
+                        required
+                        id="outlined-required"
+                        label="Login"
+                        defaultValue=""
+                        style={{marginRight: "15px"}}
+                        value={username}
+                        onChange={(e)=>setUsername(e.target.value)}
+                        />
+                        <FormControl style={{marginLeft: "15px"}} variant="outlined">
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                                <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={showPassword ? 'text' : 'password'}
+                                value={password}
+                                onChange={(e)=>setPassword(e.target.value)}
+                                endAdornment={
+                                    <InputAdornment position="end">
+                                        <IconButton
+                                                aria-label="toggle password visibility"
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                                edge="end"
+                                            >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Password"
+                                />
+                        </FormControl>
+                    </div>
+
+                    <Button variant="contained" onClick={() => {
+                        checkPassword(username, password)
+                    }}>Login</Button> 
+
+                    {incorrectLogin ? 
+                        <Typography color={red[400]} variant='body2'>
+                            Incorrect username or password
+                        </Typography> : <p></p>}
+                    
+                
+
                 </div>
-                <div style={{marginBottom : "15px"}}>
-                    <TextField
-                    required
-                    id="outlined-required"
-                    label="Login"
-                    defaultValue=""
-                    style={{marginRight: "15px"}}
-                    value={username}
-                    onChange={(e)=>setUsername(e.target.value)}
-                    />
-                    <FormControl style={{marginLeft: "15px"}} variant="outlined">
-                        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-                            <OutlinedInput
-                            id="outlined-adornment-password"
-                            type={showPassword ? 'text' : 'password'}
-                            value={password}
-                            onChange={(e)=>setPassword(e.target.value)}
-                            endAdornment={
-                                <InputAdornment position="end">
-                                    <IconButton
-                                            aria-label="toggle password visibility"
-                                            onClick={handleClickShowPassword}
-                                            onMouseDown={handleMouseDownPassword}
-                                            edge="end"
-                                        >
-                                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                                    </IconButton>
-                                </InputAdornment>
-                            }
-                            label="Password"
-                            />
-                    </FormControl>
-                </div>
-
-                <Button variant="contained" onClick={() => {
-                    checkPassword(username, password)
-                }}>Login</Button> 
-
-                {incorrectLogin ? 
-                    <Typography color={red[400]} variant='body2'>
-                        Incorrect username or password
-                    </Typography> : <p></p>}
-
-                {loggedIn ? <p>Logged in</p> : <p></p>}
-
-
+                }
             </div>
     )
 }
